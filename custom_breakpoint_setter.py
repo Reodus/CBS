992a133246c970f5321c8ee215b56e5abeb5fa90
Reo <reo@reodus.com>
2024-07-07 02:35:48 +0330

    feat: Added ability to switch between enabled and disabled breakpoints. Also added the ability to remove custom breakpoints.

--- custom_breakpoint_setter.py
+++ custom_breakpoint_setter.py
@@ -27,35 +27,47 @@ class CustomBreakPointPlugin(idaapi.PluginForm):
             item = QtWidgets.QTableWidgetItem(instruction)
             self.table.setItem(index, 0, item)
 
+        layout.addWidget(self.table)
+
         # Create input field for new instruction
+        input_layout = QtWidgets.QHBoxLayout()
         self.instruction_input = QtWidgets.QLineEdit()
         self.instruction_input.setPlaceholderText("Enter new instruction")
         self.instruction_input.returnPressed.connect(self.add_instruction)  # Add instruction on Enter key press
 
+        # Create combobox for enable/disable/remove options
+        self.status_combobox = QtWidgets.QComboBox()
+        self.status_combobox.addItems(["Disable", "Enable", "Remove Breakpoints"])
+
         # Create button to add new instruction
         self.add_instruction_btn = QtWidgets.QPushButton("Add Instruction")
         self.add_instruction_btn.clicked.connect(self.add_instruction)
+
+        layout.addLayout(input_layout)
 
         # Create button to delete selected instruction
         self.delete_instruction_btn = QtWidgets.QPushButton("Delete Selected")
         self.delete_instruction_btn.clicked.connect(self.delete_instruction)
 
         # Create set breakpoint button
-        self.set_bp_btn = QtWidgets.QPushButton("Set Breakpoints")
+        self.set_bp_btn = QtWidgets.QPushButton("Execute")
         self.set_bp_btn.clicked.connect(self.set_breakpoints)
 
         # Add keyboard shortcuts
         self.add_instruction_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence("Return"), self.parent)
         self.add_instruction_shortcut.activated.connect(self.add_instruction)
 
+        # self.add_instruction_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence("Enter"), self.parent)
+        # self.add_instruction_shortcut.activated.connect(self.add_instruction)
+
         self.delete_instruction_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence("Delete"), self.parent)
         self.delete_instruction_shortcut.activated.connect(self.delete_instruction)
 
-        layout.addWidget(self.table)
-        layout.addWidget(self.instruction_input)
-        layout.addWidget(self.add_instruction_btn)
         layout.addWidget(self.delete_instruction_btn)
         layout.addWidget(self.set_bp_btn)
+        input_layout.addWidget(self.instruction_input)
+        input_layout.addWidget(self.status_combobox)
+        input_layout.addWidget(self.add_instruction_btn)
 
         self.parent.setLayout(layout)
 
@@ -84,24 +96,39 @@ class CustomBreakPointPlugin(idaapi.PluginForm):
             if item is not None:  # Check if item exists
                 selected_instructions.append(item.text())
 
-        # Set breakpoints on selected instructions
+        status = self.status_combobox.currentText()
+
+        # Set/enable/disable breakpoints based on selected instructions
         for seg_ea in idautils.Segments():
             for func_ea in idautils.Functions(seg_ea, idc.get_segm_end(seg_ea)):
                 func_end = idc.get_func_attr(func_ea, idc.FUNCATTR_END)
+
                 for head in idautils.Heads(func_ea, func_end):
                     if idc.is_code(idc.get_full_flags(head)):
+
                         if idc.print_insn_mnem(head) in selected_instructions:
                             idaapi.add_bpt(head)
-                            idaapi.disable_bpt(head)
-                            print(f"[CBS] Breakpoint -> {hex(head)}: {idc.print_insn_mnem(head)}")
+                            
+                            msg = ""
+
+                            if status == "Disable":
+                                idaapi.disable_bpt(head)
+                                msg = "Disabled"
+                            elif status == "Enable":
+                                idaapi.enable_bpt(head)
+                                msg = "Enabled"
+                            elif status == "Remove Breakpoints":
+                                idaapi.del_bpt(head)
+                                msg = "Removed"
+                            print(f"[CBS] ({msg}) breakpoint -> {hex(head)}: {idc.print_insn_mnem(head)}")
         
     def OnClose(self, form):
         pass
 
 class MyPlugin(idaapi.plugin_t):
     flags = idaapi.PLUGIN_UNL
-    comment = "Set disabled breakpoints on specific instructions"
-    help = "This plugin sets disabled breakpoints on specified instructions"
+    comment = "Set breakpoints on specific instructions with enable/disable options"
+    help = "This plugin sets breakpoints on specified instructions with enable/disable options"
     wanted_name = "Custom Breakpoint Setter"
 
     def init(self):
